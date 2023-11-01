563f6be5015587230a381d7ee37625f54ab321d3
Giovanna Franco <rm553701@fiap.com.br>
2023-11-01 01:22:56 -0300

    Exercício CP 02

---
+++ animals.js
@@ -0,0 +1,10 @@
+//Itens 4, 5, e 6.
+
+const animals = ["gato", "papagaio", "cavalo", "tartaruga", "leão", "ovelha", "peixe"];
+
+if (!animals.includes("leão")) {
+    console.log("Leão não existe no array animals.");
+
+} else {
+    console.log("Existe um leão no array animals.");
+}
--- series.js
+++ series.js
@@ -1,1 +1,6 @@
-const my3FavoriteTVShows = ["Gilmore Girls", "B99", ""]
+// CP 02 - WEB DEVOLPMENT 
+// Giovanna Franco - ESPB 
+// Itens 1, 2 e 3.
+
+const my3FavoriteTVShows = ["Gilmore Girls", "B99", "Bom dia< Verônica"];
+console.log(my3FavoriteTVShows.length===3);
